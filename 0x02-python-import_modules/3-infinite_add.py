9e5ece1e48c4f6be39b5fbe1f73c8b7f5b22d5a1
root <you@example.com>
2024-10-21 13:31:42 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     total_sum = 0
     for arg in sys.argv[1:]:
-    total_sum += int(arg)
+        total_sum += int(arg)
 
-            print(total_sum)
+    print(total_sum)
