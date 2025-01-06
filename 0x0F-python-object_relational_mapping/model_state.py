649661b02a3b725eac87e0ff04a39fc9d5c7b400
root <you@example.com>
2025-01-06 13:29:14 -0800

    6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, create_engine 
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
