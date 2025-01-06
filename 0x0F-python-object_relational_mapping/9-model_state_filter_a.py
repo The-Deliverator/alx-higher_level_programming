b33beaabc87aee55dd16cdd9ef787b737c3136e1
root <you@example.com>
2025-01-06 15:41:07 -0800

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+create a configured session class
+create engine that connects to mysql server on localhost
+create a session
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
